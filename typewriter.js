1aac5f3eaa5e862e9541d0de1e467934262211c5
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-15 15:38:20 -0700

    typewriter with setTimeout

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+
+let count = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, count += 50)
+};
+
+
